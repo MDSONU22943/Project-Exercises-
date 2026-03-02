74733e0a29f6ce72d640043358ac22ce4f718e27
Md Sonu <mdsonu22943@gmail.com>
2026-03-02 12:27:50 +0530

    twenty-one commit

---
+++ Project-Exercise-21/src/App.jsx
@@ -0,0 +1,14 @@
+import { FaReact } from 'react-icons/fa'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Hello World</h1>
+      <FaReact color='blue' size={50} />
+    </>
+  )
+}
+
+export default App
